6f9e1f13b9d9afd96e7ba817476cc1c2e9024888
bakuyy <sophieyang12345@gmail.com>
2025-01-12 22:35:27 -0500

    Created User registration view and registration serializer

--- backend/backend/auth_app/views.py
+++ backend/backend/auth_app/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import serializers
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework import status
 
-# Create your views here.
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["username", "email", "password"]
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username = validated_data["username"],
+            email = validated_data["email"],
+            password = validated_data["password"]
+        )
+        return user
+    
+    class UserRegistrationView(APIView):
+        def post(self, request):
+            serializer = UserRegistrationSerializer(data= request.data)
+            if serializer.is_valid():
+                user = serializer.save()
+
+                refresh = RefreshToken.for_user(user)
+                access_token = str(refresh.access_token)
+
+                return Response({
+                    'refresh': str(refresh),
+                    'access': access_token
+                }, status=status.HTTP_201_CREATED)
+            
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
