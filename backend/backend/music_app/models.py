4c017319b981aacc38e958d3e191ad00e6c15eaa
bakuyy <sophieyang12345@gmail.com>
2025-01-14 00:31:26 -0500

    Updated Song and Playlist models to include Hashtag, Created Corresponding Serializers for SongSpec, Song, and Playlists

--- backend/backend/music_app/models.py
+++ backend/backend/music_app/models.py
@@ -1,16 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class Hashtag(models.Model):
+    tag = models.CharField(max_length=50)
+
 class Song(models.Model):
     song_name=models.CharField(max_length=100)
     artist_name = models.CharField(max_length=50)
     genre = models.CharField(blank=True)
-    hashtags = models.TextField(blank=True,null=True)
+    hashtags = models.ManyToManyField(Hashtag, blank=True,null=True)
     audio_file = models.FileField()
 
 class Playlist(models.Model):
     title = models.CharField(max_length=100)
     date_created = models.DateTimeField()
+    hashtags = models.ManyToManyField(Hashtag, blank=True,null=True)
     songs = models.ManyToManyField(Song, related_name="playlist songs")
 
 class SongSpecs(models.Model):
@@ -18,6 +23,4 @@ class SongSpecs(models.Model):
     danceability = models.IntegerField(blank=True)
     energy = models.IntegerField(blank=True)
     valence = models.IntegerField(blank=True)
-
-
 
---
+++ backend/backend/music_app/serializers.py
@@ -0,0 +1,74 @@
+from rest_framework import serializers
+from .models import SongSpecs, Playlist, Song, Hashtag
+
+class HashtagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Hashtag
+        fields = ["tag"]
+
+class SongSerializer(serializers.ModelSerializer):
+    hashtags = HashtagSerializer(many=True)
+    class Meta:
+        model = Song
+        fields = ["song_name", "artist_name","genre", "hashtags", "audio_file"]
+        # or fields=["__all__"]
+
+        def create(self, validated_data):
+
+            song = Song.objects.create(**validated_data)
+
+            hashtags_data = validated_data.pop('hashtags',[]) # will return empty array by default if hashtags doesn't exist
+            for hashtag_data in hashtags_data:
+                hashtag, _ = Hashtag.objects.get_or_create(name=hashtag_data["tag"])
+                song.hashtags.add(hashtag)
+
+            return song
+
+
+class SongSpecsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SongSpecs
+        fields = ["__all__"]
+
+class PlaylistSerializer(serializers.ModelSerializer):
+    hashtags = HashtagSerializer(many= True)
+    songs = SongSerializer(many=True)
+    class Meta:
+        model = Playlist
+        fields = ["__all__"]
+    
+    def create(self, validated_data):
+        playlist = Playlist.objects.create(**validated_data)
+
+        hashtags_data = validated_data.pop('hashtags',[])
+
+        for hashtag_data in hashtags_data:
+                hashtag, _ = Hashtag.objects.get_or_create(name=hashtag_data["tag"])
+                playlist.hashtags.add(hashtag)
+        return playlist
+    
+    def update(self, instance, validated_data):
+        songs_data = validated_data.pop("songs",[])
+        # updates the fields that aren't affected (i.e hashtags)
+        instance = super().update(instance, validated_data)
+
+        if songs_data:
+            #get current songs in the playlist
+            curr_song_ids = instance.songs.values_list("id", flat=True)
+            new_song_ids = {song_data['id'] for song_data in songs_data}
+
+            for song_data in songs_data:
+                if song_data["id"] not in curr_song_ids:
+                    song = Song.objects.get(id=song_data["id"])
+                    instance.songs.add(song)
+            
+            for song_id in curr_song_ids:
+                if song_id not in new_song_ids:
+                    song = Song.objects.get(id=song_id)
+                    instance.songs.remove(song)
+
+        return instance
+
+
+
+    
