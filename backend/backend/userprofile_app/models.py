e7e19363bc545cd4cabb44cdb8a95b1546510dfd
bakuyy <sophieyang12345@gmail.com>
2025-01-28 02:33:19 -0500

    Added User Profile models and views

--- backend/backend/userprofile_app/models.py
+++ backend/backend/userprofile_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)  # Make it nullable for now
+    profile_desc = models.CharField(max_length=200)
+    preferred_genres = models.JSONField(blank=True, default=list)
+
+    def __str__(self):
+        return self.user.username if self.user else "No User"
+
