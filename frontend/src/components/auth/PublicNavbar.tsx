d24beab0fb5f3f8d22d9532637ef51a545a85287
bakuyy <sophieyang12345@gmail.com>
2025-01-27 19:46:48 -0500

    Added Public Navbar and styling

---
+++ frontend/src/components/auth/PublicNavbar.tsx
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "../../../styling/PublicNavbar.css";
+
+interface NavItem {
+    title: string;
+    id:string;
+    link:string;
+
+}
+
+const PublicNavbar: React.FC = () => {
+  const links: NavItem[]= [
+    { title: "login", id: "login", link: "/login" },
+    { title: "register", id: "register", link: "/register" },
+  ]
+
+  const handleClick = () => {}
+  return (
+      <div className="navbar-links">
+        {links.map(({ title, link }) => (
+          <Link
+            key={title}
+            to={link}
+            className="navbar-link"
+            onClick={handleClick}
+          >
+            {title}
+          </Link>
+        ))}
+    </div>
+  )
+}
+
+export default PublicNavbar;
--- frontend/src/pages/public/Home.tsx
+++ frontend/src/pages/public/Home.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import "../../../styling/Home.css"
+import PublicNavbar from '../../components/auth/PublicNavbar';
 
 
 import HomePageGirl from "../../assets/images/HomePageGirl.png"
@@ -12,14 +13,16 @@ const Home: React.FC = () => {
         navigate("/login")
     }
   return (
-    <div className='home-container'>
+    <div className=''>
+        <PublicNavbar/>
+        <div className='home-container'>
         <img className="home-girl" src={HomePageGirl}/>
         <div>
             <div className='home-title'>the <span className="music-note">♫</span> world at your fingertips.</div>
             <div className='home-subtitle'>a tinder-like experience on your music</div>
             <div className='home-text' onClick={handleClick}>start swiping <span className="arrow">→</span></div>
         </div>
-
+        </div>
 
     </div>
   );
